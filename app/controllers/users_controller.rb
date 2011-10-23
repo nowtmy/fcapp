4ea63be6db986932b29261644dd9b77e5d458935
Irfan Rizvi <irfan@welltreat.us>
2011-10-23 20:05:35 +0600

    contact module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,15 +43,8 @@ class UsersController < ApplicationController
   # POST /users.xml
   def create
     @user = User.new(params[:user])
-
-    respond_to do |format|
       if @user.save
-        format.html { redirect_to('http://fcapp.heroku.com', :notice => 'User was successfully created.') }
-        format.xml  { render :xml => @user, :status => :created, :location => @user }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
+            flash[:notice] = "Successfully created user."
     end
   end
 
